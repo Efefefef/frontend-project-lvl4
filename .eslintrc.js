340f4a0576f92aceb30b6e9a891762b0817cf84e
andrey klimenok <jaguarislove@gmail.com>
2020-09-10 17:07:40 +0200

    adjust eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: 'babel-eslint',
+    parserOptions: {
+      sourceType: "module",
+    },
+  plugins: [
+    'import',
+    'react',
+    'jsx-a11y',
+    'jest'
+  ],
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings'
+  ],
+  env: {
+    'node': true,
+    'jest': true,
+    'browser': true
+  },
+  rules: {
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'no-shadow': 'off',
+    'no-console': 'off',
+    'react/prop-types': 'off'
+  }
+}
