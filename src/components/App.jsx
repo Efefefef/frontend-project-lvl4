a4566fe09086787122536eb0eb9cff2de4ac2bbc
andrey klimenok <jaguarislove@gmail.com>
2020-09-17 02:07:39 +0200

    use useContext hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
 import Context from '../context';
@@ -11,7 +11,7 @@ const renderModal = ({ modalInfo, hideModal }) => {
     return null;
   }
   const Component = getModal(modalInfo.type);
-  return <Component modalInfo={modalInfo} hideModal={hideModal} />;
+  return <Component modalInfo={modalInfo} hideModal={hideModal}/>;
 };
 
 const App = () => {
@@ -27,43 +27,39 @@ const App = () => {
   const hideModal = () => setModalInfo({ type: null, channel: null });
   const showModal = (type, channel = null) => setModalInfo({ type, channel });
 
+  const name = useContext(Context);
+
   return (
-    <Context.Consumer>
-      {(name) => (
-        <div className='row h-100 pb-3'>
-          {renderModal({ modalInfo, hideModal })}
-          <div className='col-3 border-right'>
-            <div className='d-flex mb-2'>
-              <span>Channels</span>
-              <button
-                className='btn btn-link p-0 ml-auto'
-                onClick={() => showModal('add')}
-              >+
-              </button>
-            </div>
-            <ul className='nav flex-column nav-pills nav-fill'>
-              {channels.map((channel) => (
-                <Channel channel={channel} key={channel.name} showModal={showModal}/>
-              ))}
-            </ul>
+    <div className='row h-100 pb-3'>
+      {renderModal({ modalInfo, hideModal })}
+      <div className='col-3 border-right'>
+        <div className='d-flex mb-2'>
+          <span>Channels</span>
+          <button
+            className='btn btn-link p-0 ml-auto'
+            onClick={() => showModal('add')}
+          >+
+          </button>
+        </div>
+        <ul className='nav flex-column nav-pills nav-fill'>
+          {channels.map((channel) => (
+            <Channel channel={channel} key={channel.name} showModal={showModal}/>
+          ))}
+        </ul>
+      </div>
+      <div className='col h-100'>
+        <div className='d-flex flex-column h-100'>
+          <div id='message-box' className='chat-messages overflow-auto mb-3'>
+            {messagesOnCurrentChannel.map(({ id, nickname, body }) => <div key={id}>
+              <b>{nickname}</b>: {body}
+            </div>)}
           </div>
-          <div className='col h-100'>
-            <div className='d-flex flex-column h-100'>
-              <div id='message-box' className='chat-messages overflow-auto mb-3'>
-                {messagesOnCurrentChannel.map(({ id, nickname, body }) => (
-                  <div key={id}>
-                    <b>{nickname}</b>: {body}
-                  </div>
-                ))}
-              </div>
-              <div className='mt-auto'>
-                <MessageForm currentChannelId={currentChannelId} name={name}/>
-              </div>
-            </div>
+          <div className='mt-auto'>
+            <MessageForm currentChannelId={currentChannelId} name={name}/>
           </div>
         </div>
-      )}
-    </Context.Consumer>
+      </div>
+    </div>
   );
 };
 
